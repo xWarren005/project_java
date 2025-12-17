076dae20bb915a5b7cabfcaf83d085b45bf88a70
PHONG PHU <phuvnp2882@ut.edu.vn>
2025-12-18 03:01:38 +0700

    correct Retrofit singleton and configuration

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/source/remote/ApiService.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/source/remote/ApiService.java
@@ -3,15 +3,16 @@ package com.example.s2o_mobile.data.source.remote;
 import com.example.s2o_mobile.utils.Constants;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiService {
+    private static Retrofit retrofit;
 
-    private Retrofit retrofit;
-
-    public Retrofit getClient() {
-        if (retrofit != null) {
+    public static Retrofit getClient() {
+        if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(Constants.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
         return retrofit;
