78d59fd04b1124f29278196d50c50afceab2a3ee
phucvh2379-collab <phucvh2379@ut.edu.vn>
2025-12-18 22:18:29 +0700

    Khai báo LiveData để xử lý trạng thái logout và lỗi

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/ui/auth/logout/LogoutViewModel.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/ui/auth/logout/LogoutViewModel.java
@@ -11,18 +11,8 @@ import com.example.s2o_mobile.data.repository.AuthRepository;
 import com.example.s2o_mobile.utils.SessionManager;
 public class LogoutViewModel extends AndroidViewModel {
     private final AuthRepository authRepository;
-    public LogoutViewModel(@NonNull Application application) {
-        super(application);
-
-        SessionManager sessionManager = new SessionManager(application);
-        authRepository = AuthRepository.getInstance(sessionManager);
-    }
-}
-
-public class LogoutViewModel extends AndroidViewModel {
-    private final AuthRepository authRepository;
-    public MutableLiveData<Boolean> logoutResult = new MutableLiveData<>();
-    public MutableLiveData<String> errorMessage = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> logoutResult = new MutableLiveData<>();
+    private final MutableLiveData<String> errorMessage = new MutableLiveData<>();
     public LogoutViewModel(@NonNull Application application) {
         super(application);
 
