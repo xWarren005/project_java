d8a5cbdbb02c6c1b19b5055c92e9f09a7f43c8eb
PHONG PHU <phuvnp2882@ut.edu.vn>
2025-12-18 14:48:11 +0700

    update LoginResponse model fields

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/model/LoginResponse.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/model/LoginResponse.java
@@ -1,11 +1,12 @@
 package com.example.s2o_mobile.data.model;
 
 public class LoginResponse {
-    public String token;
-    public int id;
-    public String fullName;
+
+    public String toekn;
+    public String id;
+    public String full_name;
     public String email;
     public String phone;
-    public String avatarUrl;
-    public String role;
+    public String avatar;
+    public String roles;
 }
