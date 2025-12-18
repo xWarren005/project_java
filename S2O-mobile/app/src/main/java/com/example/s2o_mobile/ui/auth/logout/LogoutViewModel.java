a7705da9329248881bcd747dcf6086482ce63f39
phucvh2379-collab <phucvh2379@ut.edu.vn>
2025-12-18 21:54:44 +0700

    Update LiveData cho trạng thái logout

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/ui/auth/logout/LogoutViewModel.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/ui/auth/logout/LogoutViewModel.java
@@ -4,11 +4,24 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
 
 import com.example.s2o_mobile.data.repository.AuthRepository;
 import com.example.s2o_mobile.utils.SessionManager;
 public class LogoutViewModel extends AndroidViewModel {
     private final AuthRepository authRepository;
+    public LogoutViewModel(@NonNull Application application) {
+        super(application);
+
+        SessionManager sessionManager = new SessionManager(application);
+        authRepository = AuthRepository.getInstance(sessionManager);
+    }
+}
+
+public class LogoutViewModel extends AndroidViewModel {
+    private final AuthRepository authRepository;
+    public MutableLiveData<Boolean> logoutResult = new MutableLiveData<>();
+    public MutableLiveData<String> errorMessage = new MutableLiveData<>();
     public LogoutViewModel(@NonNull Application application) {
         super(application);
 
