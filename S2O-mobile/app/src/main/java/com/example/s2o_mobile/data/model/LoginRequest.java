d85e4fa9b9be62c9c2f49efd02ef4b15dcfccf78
PHONG PHU <phuvnp2882@ut.edu.vn>
2025-12-18 13:59:50 +0700

    add LoginRequest model for authentication

---
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/model/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.example.s2o_mobile.data.model;
+
+public class LoginRequest {
+    public String login;
+    public String password;
+
+    public LoginRequest(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
