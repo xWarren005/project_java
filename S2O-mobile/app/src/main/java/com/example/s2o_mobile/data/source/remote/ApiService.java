1f15839ca8bf059f1a34c5e1fdc8668b177a04b5
ThinhHuynh05 <kaihuynh031105@gmail.com>
2025-12-19 12:03:35 +0700

    Update đăng ký hoạt động

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/source/remote/ApiService.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/source/remote/ApiService.java
@@ -1,20 +1,18 @@
 package com.example.s2o_mobile.data.source.remote;
 
-import com.example.s2o_mobile.utils.Constants;
-
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiService {
-    private static Retrofit retrofit;
 
-    public static Retrofit getClient() {
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
-        return retrofit;
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
+    private static ApiService instance;
+    private final Retrofit retrofit;
+
+    private ApiService() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
 }
