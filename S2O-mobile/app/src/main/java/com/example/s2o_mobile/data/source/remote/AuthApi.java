fffac6e17a232b020ea8ac7d5dec1f9d1c275b77
PHONG PHU <phuvnp2882@ut.edu.vn>
2025-12-19 22:54:51 +0700

    fix and update AuthApi.java

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/source/remote/AuthApi.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/source/remote/AuthApi.java
@@ -1,6 +1,7 @@
 package com.example.s2o_mobile.data.source.remote;
 
 import com.example.s2o_mobile.data.model.AuthResponse;
+import com.example.s2o_mobile.data.model.User;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
@@ -9,17 +10,21 @@ import retrofit2.http.POST;
 
 public interface AuthApi {
 
+
+    @FormUrlEncoded
     @POST("api/mobile/auth/login")
     Call<AuthResponse> login(
             @Field("username") String username,
             @Field("password") String password
     );
 
+    @FormUrlEncoded
     @POST("api/mobile/auth/register")
     Call<User> register(
             @Field("username") String username,
             @Field("password") String password,
             @Field("fullName") String fullName,
-            @Field("email") String email
+            @Field("email") String email,
+            @Field("phone") String phone
     );
 }
