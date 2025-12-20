6ac617c06566f59662cfee9566db7197e3cdafeb
phucvh2379-collab <phucvh2379@ut.edu.vn>
2025-12-20 19:17:33 +0700

    Tạo RestaurantApi cho chức năng lấy dữ liệu nhà hành

---
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/source/remote/RestaurantApi.java
@@ -0,0 +1,35 @@
+package com.example.s2o_mobile.data.source.remote;
+
+import com.example.s2o_mobile.data.model.Restaurant;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface RestaurantApi {
+
+    @GET("restaurant")
+    Call<List<Restaurant>> getRestaurants(
+            @Query("page") int page,
+            @Query("size") int size
+    );
+
+    @GET("restaurants/search")
+    Call<List<Restaurant>> searchRestaurants(
+            @Query("keyword") String keyword
+    );
+
+    @GET("restaurants/detail")
+    Call<Restaurant> getRestaurantDetail(
+            @Query("id") String restaurantId
+    );
+
+    @GET("restaurants/featured")
+    Call<Restaurant> getFeaturedRestaurants();
+
+    @GET("restaurants/recommended")
+    Call<Restaurant> getRecommendedRestaurants();
+}
