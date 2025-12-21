7d15a67617ef777c23dea1e7c16150cbc7f16ee0
PHONG PHU <phuvnp2882@ut.edu.vn>
2025-12-21 22:53:30 +0700

    add livedata fields for home screen

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/ui/home/HomeViewModel.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/ui/home/HomeViewModel.java
@@ -3,14 +3,26 @@ package com.example.s2o_mobile.ui.home;
 import android.app.Application;
 
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.MutableLiveData;
 
+import com.example.s2o_mobile.data.model.Restaurant;
 import com.example.s2o_mobile.data.repository.RestaurantRepository;
+
+import java.util.List;
 
 public class HomeViewModel extends ViewModel {
 
     private RestaurantRepository restaurantRepository;
 
+    public MutableLiveData<Boolean> loading;
+    public MutableLiveData<String> errorMessage;
+    public MutableLiveData<List<Restaurant>> restaurants;
+
     public HomeViewModel(Application application) {
         restaurantRepository = new RestaurantRepository();
+    }
+
+    public MutableLiveData getRestaurants() {
+        return restaurants;
     }
 }
