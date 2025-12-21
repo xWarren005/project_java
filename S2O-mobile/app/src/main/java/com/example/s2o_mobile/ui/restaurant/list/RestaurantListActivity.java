e5160e20d25dd62f7b42129d835e528adfdcaf26
ThinhHuynh05 <kaihuynh031105@gmail.com>
2025-12-21 17:27:20 +0700

    update màn hình hiển thi danh sách nhà hàng

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/ui/restaurant/list/RestaurantListActivity.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/ui/restaurant/list/RestaurantListActivity.java
@@ -5,26 +5,39 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.s2o_mobile.R;
+import com.example.s2o_mobile.data.model.Restaurant;
 
+
+import java.util.ArrayList;
 public class RestaurantListActivity extends AppCompatActivity {
 
     protected RecyclerView recyclerView;
     protected ProgressBar progress;
     protected TextView txtEmpty;
+    protected RestaurantAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_list);
         bindViews();
+        setupRecycler();
     }
 
     protected void bindViews() {
         recyclerView = findViewById(R.id.recyclerRestaurants);
         progress = findViewById(R.id.progress);
         txtEmpty = findViewById(R.id.txtEmpty);
+    }
+    protected void setupRecycler() {
+        adapter = new RestaurantAdapter(new ArrayList<>(), restaurant -> {
+        });
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
