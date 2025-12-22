a1bd5602e7b1bbb2baefd657db23f9bf3f0d81f0
PHONG PHU <phuvnp2882@ut.edu.vn>
2025-12-23 00:55:12 +0700

    load restaurant detail in ViewModel

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/ui/restaurant/detail/RestaurantDetailViewModel.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/ui/restaurant/detail/RestaurantDetailViewModel.java
@@ -4,8 +4,11 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.s2o_mobile.data.model.Restaurant;
+import com.example.s2o_mobile.data.repository.RestaurantRepository;
 
 public class RestaurantDetailViewModel extends ViewModel {
+
+    private RestaurantRepository restaurantRepository;
 
     public MutableLiveData<Boolean> loading;
     public MutableLiveData<String> errorMessage;
@@ -24,5 +27,21 @@ public class RestaurantDetailViewModel extends ViewModel {
 
     public MutableLiveData<Restaurant> getRestaurant() {
         return restaurant;
+    }
+
+    public void loadRestaurantDetail(int restaurantId) {
+
+        if (restaurantId < 0) {
+            errorMessage.setValue("ID hợp lệ");
+        }
+
+        restaurantRepository.getRestaurantDetail(
+                0,
+                null,
+                errorMessage,
+                loading
+        );
+
+        loading.setValue(false);
     }
 }
