0e794d2967d442ffa6b00147193690373291e4cb
xWarren005 <trantrongquan2005@gmail.com>
2025-12-23 09:55:59 +0700

    config: Cấu hình Security cho phép truy cập tài nguyên tĩnh (css/js)

--- S2O-web/src/main/java/com/s2o/app/S2oApplication.java
+++ S2O-web/src/main/java/com/s2o/app/S2oApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class S2oApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(S2oApplication.java, args);
+        SpringApplication.run(S2oApplication.class, args);
     }
 
 }
