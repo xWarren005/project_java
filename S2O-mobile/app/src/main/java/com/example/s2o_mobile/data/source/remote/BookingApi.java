91c14f8e37e6916e6a6a79afd1a14450d191e456
ThinhHuynh05 <kaihuynh031105@gmail.com>
2025-12-24 23:30:11 +0700

    Update chức năng booking

---
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/source/remote/BookingApi.java
@@ -0,0 +1,12 @@
+package com.example.s2o_mobile.data.source.remote;
+
+import com.example.s2o_mobile.data.model.Reservation;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
