4c72e67fa70252360bd054e6f5989beee31f189f
Lê Văn Giang <giangl9314@gmail.com>
2025-12-27 00:48:22 +0700

    cập nhật User tìm bàn theo restaurantID

--- S2O-web/src/main/java/com/s2o/app/controller/WelcomeController.java
+++ S2O-web/src/main/java/com/s2o/app/controller/WelcomeController.java
@@ -1,14 +1,41 @@
 package com.s2o.app.controller;
 
+import com.s2o.app.entity.RestaurantTable;
+import com.s2o.app.repository.TableRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
 public class WelcomeController {
+    private final TableRepository tableRepository;
+    public WelcomeController(TableRepository tableRepository) {
+        this.tableRepository = tableRepository;
+    }
     @GetMapping("/welcome")
-    public String welcomePage() {
+    public String welcomePage(
+            @RequestParam("tableId") Integer tableId,
+            HttpSession session,
+            Model model
+    ) {
+        RestaurantTable table = tableRepository.findById(tableId)
+                .orElse(null);
+        if (table == null) {
+            model.addAttribute("error", "Bàn không tồn tại");
+            return "error";
+        }
+        // 2. Lưu thông tin bàn + nhà hàng vào session
+        session.setAttribute("TABLE_ID", table.getId());
+        session.setAttribute("TABLE_NAME", table.getTableName());
+        session.setAttribute("RESTAURANT_ID", table.getRestaurantId());
+// 3. Truyền dữ liệu ra view
+        model.addAttribute("tableName", table.getTableName());
+        model.addAttribute("restaurantId", table.getRestaurantId());
+
         return "user/welcome";
     }
 }
