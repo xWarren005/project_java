b829692a23f747fe4604f86ebfcc61f88a86caf3
PHONG PHU <phuvnp2882@ut.edu.vn>
2025-12-26 22:44:27 +0700

    Update MenuItem model but still incomplete

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/model/MenuItem.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/model/MenuItem.java
@@ -1,6 +1,8 @@
 package com.example.s2o_mobile.data.model;
 
-public class MenuItem {
+import java.io.Serializable;
+
+public class MenuItem implements Serializable {
 
     private int id;
     private String name;
@@ -10,14 +12,19 @@ public class MenuItem {
     private String description;
     private String category;
 
-    public MenuItem(int id, String name, int price) {
+    public MenuItem(int id, String name, int price, String imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.imageUrl = imageUrl;
     }
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -26,5 +33,10 @@ public class MenuItem {
 
     public int getPrice() {
         return price;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem{id=" + id + ", name='" + name + "'}";
     }
 }
