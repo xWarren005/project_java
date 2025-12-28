fd28ae6e6f29c1273199647d76ed8bb9439cdc87
PHONG PHU <phuvnp2882@ut.edu.vn>
2025-12-28 23:56:39 +0700

    add order items and basic fields

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/model/Order.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/model/Order.java
@@ -1,30 +1,44 @@
 package com.example.s2o_mobile.data.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Order implements Serializable {
 
     private int id;
     private int restaurantId;
+    private int tableId;
+
+    private List<OrderItem> items;
 
     private double totalAmount;
+    private String status;
 
     public Order() {
     }
 
-    public Order(int restaurantId) {
+    public Order(int restaurantId, int tableId, List<OrderItem> items) {
         this.restaurantId = restaurantId;
+        this.tableId = tableId;
+        this.items = items;
     }
 
-    public int getId() {
-        return id;
+    public List<OrderItem> getItems() {
+        return items;
     }
 
-    public int getRestaurantId() {
-        return restaurantId;
+    public double calcTotalAmount() {
+        double sum = 0;
+        for (OrderItem item : items) {
+            sum += item.price * item.quantity;
+        }
+        return sum;
     }
 
-    public double getTotalAmount() {
-        return totalAmount;
+    public static class OrderItem implements Serializable {
+        int menuItemId;
+        String name;
+        double price;
+        int quantity;
     }
 }
