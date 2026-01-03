d2463acb34112959a0037dd2ccb084b08d14b2d0
PHONG PHU <phuvnp2882@ut.edu.vn>
2026-01-03 20:22:12 +0700

    add rank code and discount info

--- S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/model/MemberRank.java
+++ S2O-mobile/app/src/main/java/com/example/s2o_mobile/data/model/MemberRank.java
@@ -5,13 +5,19 @@ import java.io.Serializable;
 public class MemberRank implements Serializable {
 
     private int id;
+    private String code;
     private String name;
+    private String description;
+
+    private double discountPercent;
+    private double pointMultiplier;
 
     public MemberRank() {
     }
 
-    public MemberRank(int id, String name) {
+    public MemberRank(int id, String code, String name) {
         this.id = id;
+        this.code = code;
         this.name = name;
     }
 
@@ -19,15 +25,47 @@ public class MemberRank implements Serializable {
         return id;
     }
 
+    public String getCode() {
+        return code;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public double getPointMultiplier() {
+        return pointMultiplier;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public void setCode(String code) {
+        this.code = code;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public void setPointMultiplier(double pointMultiplier) {
+        this.pointMultiplier = pointMultiplier;
     }
 }
