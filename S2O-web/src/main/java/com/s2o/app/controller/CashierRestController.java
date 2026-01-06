90011f17740d7b0af3ea1b39d86b02217918ab8c
xWarren005 <trantrongquan2005@gmail.com>
2026-01-06 23:34:50 +0700

    Tạo API Endpoint trả về JSON.

--- S2O-web/src/main/java/com/s2o/app/controller/CashierRestController.java
+++ S2O-web/src/main/java/com/s2o/app/controller/CashierRestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.s2o.app.dto.response.CashierInvoiceDTO;
 
 import java.util.List;
 
@@ -23,4 +24,11 @@ public class CashierRestController {
         Integer restaurantId = 1;
         return ResponseEntity.ok(cashierService.getTablesForCashier(restaurantId));
     }
+
+    @GetMapping("/invoices")
+    public ResponseEntity<List<CashierInvoiceDTO>> getAllInvoices() {
+        List<CashierInvoiceDTO> invoices = cashierService.getInvoicesForCashier();
+        return ResponseEntity.ok(invoices);
+    }
+
 }
